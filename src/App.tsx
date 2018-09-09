3ffee02535036a42d47e6465731546433d87c91b
renshan <renshan@renshandeMacBook-Pro.local>
2018-09-09 14:06:05 +0800

    F

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import * as XLSX from 'xlsx';
 import './App.css';
-
 import logo from './logo.svg';
 
 class App extends React.Component {
 
   private workBook: XLSX.WorkBook;
-  private sheetNames: string[];
+  private sheetNames: string[] = [];
 
   public constructor(props: any) {
     super(props)
@@ -19,33 +20,49 @@ class App extends React.Component {
     }
   }
 
+  public alert(message: string) {
+    toast(message);
+  }
+
   public onChange(e: any) {
 
     if (e.target == null || e.target.files.length === 0) {
       return;
     }
 
-    this.setState({ sheetNames: []});
+    window.console.log(e.target.files[0].name.match(/\.xls|\.xlsx|\.xlsm$/ig));
+
+    if (e.target.files[0].name.match(/\.xls|\.xlsx|\.xlsm/ig) === null) {
+      this.alert("请上传excel文件");
+      return;
+    }
+
+    this.setState({sheetNames: []});
 
     const reader = new FileReader();
-    reader.readAsBinaryString(e.target.files[0]);
-
-    window.console.log(e.target.files[0].type);
-
+    reader.readAsArrayBuffer(e.target.files[0]);
     reader.onload = (a: any) => this.read(a.target.result);
   }
 
   public read(data: any) {
 
     try {
-      this.workBook = XLSX.read(data, { type: 'binary' });
+      this.workBook = XLSX.read(data, {type: 'array', bookSheets: true, cellHTML: true});
     } catch (error) {
-      window.console.error(error)
-      window.alert("无法识别excel文件")
+      this.alert("无法识别excel文件")
       return;
     }
 
+    window.console.log(JSON.stringify(this.workBook))
+
     this.sheetNames=  this.workBook.SheetNames ? this.workBook.SheetNames : [];
+
+    this.workBook.SheetNames.forEach((sheetName) => {
+      const formulae = XLSX.utils.sheet_to_json(this.workBook.Sheets[sheetName]);
+      
+      window.console.log(formulae)
+
+		});
 
     this.setState({
       sheetNames: this.sheetNames,
@@ -60,11 +77,13 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">上传 Excel</h1>
         </header>
-        <p className="App-intro"> <input type="file" accept="application/vnd.ms-excel" onChange={ this.onChange } /></p>
+        <p className="App-intro"> <input type="file" onChange={ this.onChange } /></p>
 
         <div>
-          { this.sheetNames.map((sheet, k) => (<p key={k}>{sheet}</p>)) }
+          { this.sheetNames.length > 0 &&  this.sheetNames.map((sheet, k) => (<p key={k}>{sheet}</p>)) }
         </div>
+
+        <ToastContainer />
       </div>
     );
   }
