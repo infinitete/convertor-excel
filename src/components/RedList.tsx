e37eed3ebc396a7d777372e69e36fb00385e1aab
infinitete <adogwithfragrancy@gmail.com>
2018-09-14 15:26:22 +0800

    完成红名单提交

--- src/components/RedList.tsx
+++ src/components/RedList.tsx
@@ -1,16 +1,37 @@
 import { Button, Layout, notification, Table, Tabs } from 'antd';
+import * as hasha from 'hasha';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import {
     loadExcel
  } from '../utils/Exce';
+import { host, name, password } from '../utils/settings';
 
  interface IState {
      columns: any[],
      dataSource: any[]
      currentSheet: string|null,
-     fileName: string|null
+     fileName: string|null,
+     token: string | null
  }
+
+// interface IForm {
+//     creditCode: string,
+//     detail: string,
+//     enterpriseDomain: string
+//     enterpriseName: string
+//     joinTime: string
+//     type: string
+// }
+
+const MappedFormField = {
+    creditCode: '注册号',
+    detail: '内容',
+    enterpriseDomain: '经营范围',
+    enterpriseName: '企业名称',
+    joinTime: '创建为诚信主体时间',
+    type: '榜单类型'
+};
 
 class Redlist extends React.Component<{}, IState> {
 
@@ -24,31 +45,49 @@ class Redlist extends React.Component<{}, IState> {
         super(props);
         this.onButtonClick = this.onButtonClick.bind(this);
         this.onChange = this.onChange.bind(this);
-        this.onRow = this.onRow.bind(this);
         this.onTabsChange = this.onTabsChange.bind(this);
         this.close = this.close.bind(this);
+        this.actions = this.actions.bind(this);
+        this.onWindowRefresh = this.onWindowRefresh.bind(this);
 
         this.state = {
             columns: [],
             currentSheet: null,
             dataSource: [],
-            fileName: null
+            fileName: null,
+            token: null
         };
     }
 
     public componentDidMount() {
+
+        // 从缓存读取
         try {
             const data = window.sessionStorage.getItem(this.sessionKey);
 
             if (data != null) {
                 const cache = JSON.parse(data);
 
-                window.console.log(cache);
+                for (const sheet in cache.columns) {
+
+
+                    if (cache.columns.hasOwnProperty(sheet)) {
+                        cache.columns[sheet].pop()
+                        cache.columns[sheet].push({
+                            key: `action`,
+                            render: (_: any, record: any) => <a href="javascript:;" key={`${(Math.random()).toString(2)}`} onClick={ this.actions.bind(this, record) }>保存</a>,
+                            title: '操作',
+                        });
+                    }
+                }
 
                 this.setState({
                     columns: cache.columns,
+                    currentSheet: cache.currentSheet,
                     dataSource: cache.dataSource
                 });
+
+                window.console.log(cache.columns, cache.currentSheet, cache.dataSource);
 
                 notification.success({
                     description: '已从您刚才关闭的文件中读取',
@@ -59,6 +98,14 @@ class Redlist extends React.Component<{}, IState> {
         } catch (error) {
             window.console.log(error);
         }
+
+        // 在页面刷新前保留当前页面状态
+        window.addEventListener('unload', this.onWindowRefresh);
+    }
+
+    public onWindowRefresh() {
+        this.close();
+        window.removeEventListener('unload', this.onWindowRefresh);
     }
 
     public onButtonClick() {
@@ -92,17 +139,163 @@ class Redlist extends React.Component<{}, IState> {
             this.setState({ fileName: file.name });
         }
 
-        loadExcel(file).then((data: any) => this.setState(data));
+        loadExcel(file).then((data: any) => {
+
+            let i = 0;
+
+            for (const sheet in data.columns) {
+                if (data.columns.hasOwnProperty(sheet)) {
+
+                    if (i++ === 0) {
+                        data.currentSheet = sheet;
+                    }
+                    data.columns[sheet].push({
+                        key: `action`,
+                        render: (_: any, record: any) => <a href="javascript:;" onClick={ this.actions.bind(this, record) }>保存</a>,
+                        title: '操作',
+                    });
+                }
+            }
+
+            this.setState(data)
+        });
     }
 
-    public onRow(record: any) {
-        return {
-            onClick: (event: any) => this.onRowClick(record)
+    /**
+     * 保存操作
+     *
+     * @param record 行数据
+     */
+    public async actions(record: any) {
+
+        const cacheKey = hasha(JSON.stringify(record), {algorithm: 'sha256'});
+
+        if (window.localStorage.getItem(cacheKey)) {
+            notification.warning({
+                description: '这条记录已经保存过了',
+                message: '提示'
+            });
+            return;
         }
+
+        // 登录
+        const token = await this.login();
+
+        // 提交数据
+        this.setState({token: token.toString()}, () => this.post(record));
     }
 
-    public onRowClick(e: any) {
-        window.console.log(e)
+    public async login() {
+
+        const request = new Request(`${host}/auth/login`, {
+            body: JSON.stringify({name, password}),
+            headers: {
+                'content-type': 'application/json'
+            },
+            method: 'POST',
+        });
+
+        return new Promise((resolve, reject) => {
+            fetch(request).then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+
+                throw new Error(response.statusText);
+            }).then(json => {
+                if (json.code.toString() === '2000') {
+                    return resolve(json.obj);
+                }
+
+                window.console.log(json.message);
+
+                reject("登录失败");
+            }).catch(error => {
+                notification.error({
+                    description: error.toString(),
+                    message: '登录失败'
+                });
+                reject(error);
+            });
+        });
+    }
+
+    public post(record: any) {
+
+        const sheet  = this.state.currentSheet;
+        if (sheet === null) {
+            return;
+        }
+
+        const cacheKey = hasha(JSON.stringify(record), {algorithm: 'sha256'});
+
+        const data = {};
+        const column = this.state.columns[sheet];
+        const map = {};
+
+        column.map((c: any) => {
+            for(const key in MappedFormField) {
+                if (MappedFormField.hasOwnProperty(key)) {
+                    if (MappedFormField[key] === c.title) {
+                        window.console.log(c.dataIndex);
+                        map[c.dataIndex] = key;
+                    }
+                }
+            }
+        });
+
+        window.console.log(map);
+
+        for(const k in record) {
+            if (record.hasOwnProperty(k) && map[k] != null) {
+                Object.defineProperty(data, map[k], {
+                    enumerable: true,
+                    value: record[k]
+                })
+            }
+        }
+
+        Object.defineProperty(data, 'detail', {
+            enumerable: true,
+            value: JSON.stringify(record)
+        });
+
+        Object.defineProperty(data, 'type', {
+            enumerable: true,
+            value: '1'
+        });
+
+        const token: string = this.state.token ? this.state.token : '';
+        const request = new Request(`${host}/redBlackList`, {
+            body: JSON.stringify(data),
+            headers: {
+                'content-type': 'application/json;utf-8',
+                token
+            },
+            method: 'POST',
+            mode: 'cors'
+        });
+
+        fetch(request).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+
+            throw new Error("数据提交失败");
+        }).then(json => {
+            if (json.code.toString() === "2000") {
+
+                window.localStorage.setItem(cacheKey, JSON.stringify(record));
+
+                return notification.success({
+                    description: "提交成功",
+                    message: "提交成功",
+                    placement: 'topLeft'
+                })
+            }
+
+            throw new Error(json.toString());
+        }).catch(error =>{ window.console.log(error) })
     }
 
     public rowKey(record: any): string {
@@ -110,14 +303,19 @@ class Redlist extends React.Component<{}, IState> {
     }
 
     public onTabsChange(activeKey: any) {
-        window.console.log(activeKey, this.state.currentSheet);
+        this.setState({ currentSheet: activeKey });
     }
 
     public close() {
 
+        if (this.state.columns.length === 0) {
+            return;
+        }
+
         try {
             const data = {
                 columns: this.state.columns,
+                currentSheet: this.state.currentSheet,
                 dataSource: this.state.dataSource
             };
 
@@ -153,13 +351,14 @@ class Redlist extends React.Component<{}, IState> {
                     key={i++}
                     dataSource={ dataSource[key] }
                     columns={ columns[key] }
-                    onRow= { this.onRow }
                     rowKey={ this.rowKey }/>
                 </Tabs.TabPane>);
             }
         }
 
-      const tabs = views.length > 0 ? (<Tabs ref={ r => this.tabsRef = r } tabPosition="top" onChange={this.onTabsChange} animated={true}>{views}</Tabs>) : null;
+        const defaultActiveKey = this.state.currentSheet ? this.state.currentSheet : undefined;
+
+      const tabs = views.length > 0 ? (<Tabs ref={ r => this.tabsRef = r } defaultActiveKey={ defaultActiveKey } tabPosition="top" onChange={this.onTabsChange} animated={true}>{views}</Tabs>) : null;
         const closer = views.length > 0
             ?
             <div style={{ position: 'fixed', right: 24, top: 24 }}><Button shape="circle" type="primary" size="small" onClick={this.close} icon="close" /></div>
---
+++ src/utils/settings.tsx
@@ -0,0 +1,9 @@
+const host = 'http://58.42.227.138:28220'
+const name = 'admin';
+const password = '111111';
+
+export {
+    host,
+    name,
+    password
+};
